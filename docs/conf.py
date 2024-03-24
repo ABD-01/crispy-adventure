0ae597ee383c1fb41976fa428b2e9f1df5bde101
ABD-01 <myac931@gmail.com>
2024-03-24 13:12:55 +0530

    Added temporary video link for ROS2 and removed "documentation" word from title

--- docs/conf.py
+++ docs/conf.py
@@ -72,6 +72,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_title = f'{project} {release}'
+
 html_theme = 'furo'
 html_static_path = ['_static']
 
